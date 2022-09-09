69276985e18a744eaeae3052f8134d1a92c46ec3
tnncodes <tnncodes@gmail.com>
2022-09-09 11:02:45 -0300

    componente Menu - Parte 2

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,19 +1,38 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { SectionContainer } from '../SectionContainer';
 import { LogoLink } from '../LogoLink';
 import { NavLinks } from '../NavLinks';
+import { Menu as MenuIcon } from '@styled-icons/material-outlined/Menu';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 import * as S from './styles';
 
 export const Menu = ({ links = [], logoData }) => {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <S.Container>
-      <SectionContainer>
-        <S.MenuContainer>
-          <LogoLink {...logoData} />
-          <NavLinks links={links} />
-        </S.MenuContainer>
-      </SectionContainer>
-    </S.Container>
+    <>
+      <S.MenuButton
+        visible={visible}
+        onClick={() => setVisible(true)}
+        aria-label="Open/Close menu"
+      >
+        {visible ? (
+          <CloseIcon aria-label="Close Menu" />
+        ) : (
+          <MenuIcon aria-label="Open Menu" />
+        )}
+      </S.MenuButton>
+
+      <S.Container visible={visible} onClick={() => setVisible(false)}>
+        <SectionContainer>
+          <S.MenuContainer>
+            <LogoLink {...logoData} />
+            <NavLinks links={links} />
+          </S.MenuContainer>
+        </SectionContainer>
+      </S.Container>
+    </>
   );
 };
 
