9b2f751ea27c4f13cda6a99cb7851f724595169f
tnncodes <tnncodes@gmail.com>
2022-09-15 18:30:40 -0300

    GridText: estrutura e estilos do componente

---
+++ src/components/GridText/index.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import * as S from './styles';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+
+export const GridText = ({ title, description, grid, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <S.Container>
+        <Heading size="huge" uppercase colorDark={!background}>
+          {title}
+        </Heading>
+
+        <TextComponent>{description}</TextComponent>
+
+        <S.Grid>
+          {grid.map((el) => (
+            <S.GridElement key={el.title}>
+              <Heading size="medium" colorDark={!background}>
+                {el.title}
+              </Heading>
+              <TextComponent>{el.description}</TextComponent>
+            </S.GridElement>
+          ))}
+        </S.Grid>
+      </S.Container>
+    </SectionBackground>
+  );
+};
+
+GridText.propTypes = {
+  background: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  grid: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
